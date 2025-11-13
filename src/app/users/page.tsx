f093e233305d6da36b47aca27949f47c45274ff9
mentor <davidnwachukwum@gmail.com>
2025-11-13 09:53:51 +0100

    first commit

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+
 import { useAuthStore } from "@/app/store/authStore";
 import Footer from "@/components/footer";
 import Loading from "@/components/Loainding";
